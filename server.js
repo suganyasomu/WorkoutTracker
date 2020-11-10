8dedacce2a154385a889f5c67062d3dadbd9dd99
suganya somu <sweetsugan12@gmail.com>
2020-11-09 16:27:28 -0800

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+{  
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false 
+});
 
 const htmlRoutes = require("./routes/htmlroutes");
 const apiRoutes = require("./routes/apiroutes");
